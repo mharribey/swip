0885e700135caeb6727f5d0ed86d21b9879c0a4f
mharribey <mathieuharribey@hotmail.fr>
2017-11-09 16:55:07 +0100

    first commit

---
+++ SWiP2/BillTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BillTableViewCell.swift
+//  SWiP2
+//
+//  Created by Mathieu Harribey on 09/11/2017.
+//  Copyright © 2017 Mathieu Harribey. All rights reserved.
+//
+
+import UIKit
+
+class BillTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
